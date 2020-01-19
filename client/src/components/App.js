ceee1c6ed52e7a1dad1bfc2ff0261fb99af46d95
sergei <meepwnd33@i.ua>
2020-01-19 19:34:53 +0200

    added confirm modal on item deletion

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import HomePage from './HomePage'
-import NewsPage from './NewsPage';
-import NotFoundPage from './NotFoundPage';
-import NewsItemPage from './NewsItemPage'
-import UpdateItemPage from './UpdateItemPage';
-import SignupPage from './SignupPage';
-import LoginPage from './LoginPage';
-import Logout from './Logout';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import NewsPage from "./NewsPage";
+import NotFoundPage from "./NotFoundPage";
+import NewsItemPage from "./NewsItemPage";
+import UpdateItemPage from "./UpdateItemPage";
+import SignupPage from "./SignupPage";
+import LoginPage from "./LoginPage";
+import Logout from "./Logout";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
 
-    <Switch>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/news" component={NewsPage}/>
-      <Route exact path="/news/:id" component={NewsItemPage}/>
-      <Route path="/news/:id/edit" component={UpdateItemPage}/>
-      <Route path="/signup" component={SignupPage}/>
-      <Route path="/login" component={LoginPage}/>
-      <Route path="/logout" component={Logout}/>
-      <Route component={NotFoundPage} />
-    </Switch>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/news" component={NewsPage} />
+          <Route exact path="/news/:id" component={NewsItemPage} />
+          <Route path="/news/:id/edit" component={UpdateItemPage} />
+          <Route path="/signup" component={SignupPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/logout" component={Logout} />
+          <Route component={NotFoundPage} />
+        </Switch>
 
-    <Footer />
-  </BrowserRouter>
-);
+        {/* <Footer /> */}
+      </BrowserRouter>
+
+    </>
+  );
+};
 
 export default App;
--- client/src/components/NewsItem.js
+++ client/src/components/NewsItem.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { removeItem } from "../api/news";
 import { connect } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
+import Modal from "./Modal";
 
 const NewsItem = ({ item, fetchNews, token, userData, creator }) => {
+  const [isModalOpen, toggleModal] = useState(false);
   const isEditable = userData._id === creator;
   return (
     <div key={item._id}>
       <div className="news-item">
-        <Link to={`/news/${item._id}`} className="news-item__link">{item.title}</Link>
+        <Link to={`/news/${item._id}`} className="news-item__link">
+          {item.title}
+        </Link>
         <span hidden={!isEditable} className="news-item__icons">
-          <button className="news-item__delete"
-            onClick={() => {
-              removeItem(item._id, fetchNews, token);
-            }}
+          <button
+            className="news-item__delete"
+            onClick={() => toggleModal(!isModalOpen)}
           >
             <FontAwesomeIcon icon={faTrash} />
           </button>
@@ -23,6 +26,32 @@ const NewsItem = ({ item, fetchNews, token, userData, creator }) => {
           <Link to={`/news/${item._id}/edit`} className="news-item__edit">
             <FontAwesomeIcon icon={faEdit} />
           </Link>
+
+          {isModalOpen && (
+            <Modal>
+              <div className="modal-overlay">
+                <div className="modal-body">
+                  <div className="modal-header">
+                    <h1>Are you really want to delete this newsitem?</h1>
+                  </div>
+                  <button
+                    className="btn_confirm"
+                    onClick={() => {
+                      removeItem(item._id, fetchNews, token);
+                    }}
+                  >
+                    Confirm
+                  </button>
+                  <button
+                    className="btn_cancel"
+                    onClick={() => toggleModal(!isModalOpen)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          )}
         </span>
       </div>
 
